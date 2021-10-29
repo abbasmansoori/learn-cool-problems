b189c3367e54911cd447ffc5c1867d66dd237e5d
abbas.mansoori@iths.se <abbas.mansoori@iths.se>
2021-10-29 14:54:14 +0200

    tsting

--- problems/easy/test_even.js
+++ problems/easy/test_even.js
@@ -2,7 +2,7 @@ import assert from "assert";
 import { even } from "./even.js";
 
 describe("even", () => {
-    it("It should return [2,4]", () => {
+    it("2 or 4", () => {
         assert.deepEqual(even([1, 2, 3, 4, 5]), [2, 4]);
     });
 });
